2488bbd2182ce0b85cd041e12997b639635e7d1b
P2883280 <C-MequannintMunye.Zeru@charter.com>
2019-08-12 11:12:40 -0500

    Initial Commit

---
+++ src/main/java/com/charter/springbootJWT/model/Role.java
@@ -0,0 +1,13 @@
+package com.charter.springbootJWT.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    ROLE_ADMIN, ROLE_CLIENT;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+
+}
---
+++ src/main/java/com/charter/springbootJWT/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.charter.springbootJWT.repository;
+
+import javax.transaction.Transactional;
+
+import com.charter.springbootJWT.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    boolean existsByUsername(String username);
+
+    User findByUsername(String username);
+
+    @Transactional
+    void deleteByUsername(String username);
+
+}
